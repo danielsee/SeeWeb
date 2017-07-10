fea705324430106ae0a9f68827c44fd8f395c0ae
[daniel] <dswatanabesee@gmail.com>
2017-07-10 17:03:56 +0800

    lastest update (navigation all done)

---
+++ javascript.js
@@ -0,0 +1,56 @@
+var myIndex = 0;
+carousel();
+
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    for (i = 0; i < x.length; i++) {
+       x[i].style.display = "none";  
+    }
+    myIndex++;
+    if (myIndex > x.length) {myIndex = 1}    
+    x[myIndex-1].style.display = "block";  
+    setTimeout(carousel, 3000); /*秒數變換*/
+    }
+    
+     function self()
+    {
+      
+   alert("You have selected this page and Please try for other.")
+
+    }
+   
+    function self_chinese()
+    {
+      
+   alert(" 您正在此頁，請更換頁面.")
+
+    }
+    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent)) {
+    // Take the user to a different screen here.
+
+}
+
+/*
+(click button text script)
+
+
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  if (n > x.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";  
+  }
+  x[slideIndex-1].style.display = "block";  
+}
+
+*/
