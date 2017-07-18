f3a7ce80b5cc825f747dc3945c5a60f31b3209b9
[daniel] <dswatanabesee@gmail.com>
2017-07-18 21:47:06 +0800

    css changed latest

--- javascript.js
+++ javascript.js
@@ -1,18 +1,8 @@
 var myIndex = 0;
-carousel();
+
 detectmob();
 
-function carousel() {
-    var i;
-    var x = document.getElementsByClassName("mySlides");
-    for (i = 0; i < x.length; i++) {
-       x[i].style.display = "none";  
-    }
-    myIndex++;
-    if (myIndex > x.length) {myIndex = 1}    
-    x[myIndex-1].style.display = "block";  
-    setTimeout(carousel, 3000); /*秒數變換*/
-    }
+
      function self()
     { 
    alert("You have selected this page and Please try for other.");
