da8e0f2c2068d6993138f07cc5081de256f03ed9
[daniel] <dswatanabesee@gmail.com>
2017-07-26 22:25:56 +0800

    sadf

--- javascript.js
+++ javascript.js
@@ -24,7 +24,7 @@ detectmob();
    link.rel = 'stylesheet';
    link.href = "navigation_bar.css";
    head.appendChild(link);
-   /*alert("ilatest");*/
+   alert("ilatest");
    return link;
   } 
   
